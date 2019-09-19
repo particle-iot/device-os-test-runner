a4b81da30b3d8302cc68a97700c7ac439fd8e532
Sergey Polyakov <sergeuz@gmail.com>
2019-09-19 19:14:44 +0200

    WIP: Firmware update via control requests; initial support for control requests in the unit test library

---
+++ test/communication/keepalive/app/keepalive.cpp
@@ -0,0 +1,12 @@
+#include "application.h"
+#include "unit-test/unit-test.h"
+
+test(test1) {
+}
+
+test(test2) {
+	assertEqual(1, 2);
+}
+
+test(test3) {
+}
---
+++ test/communication/keepalive/keepalive.spec.js
@@ -0,0 +1,13 @@
+suite('Keepalive interval');
+
+platforms('gen3', 'gen2');
+
+test('test1', () => {
+});
+
+test('test2', () => {
+});
+
+test('test3', () => {
+	expect(1).to.equal(2);
+});
