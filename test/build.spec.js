a06053212c1595bb66df5f8d4455242b3d8a8640
Sergey Polyakov <sergeuz@gmail.com>
2021-12-08 15:06:09 +0200

    Fix linter errors

--- test/build.spec.js
+++ test/build.spec.js
@@ -4,31 +4,31 @@ const { Builder } = require('../lib/build');
 const sinon = require('sinon');
 
 async function initBuilder(opts) {
-  const builder = new Builder({ ...opts, log });
-  await builder.init();
-  return builder;
+	const builder = new Builder({ ...opts, log });
+	await builder.init();
+	return builder;
 }
 
 describe('Builder', () => {
-  let builder = null;
+	let builder = null;
 
-  beforeEach(async () => {
-    builder = await initBuilder({
-      testDir: fixturePath(),
-      deviceOsDir: '/path/to/device-os',
-      tempDir: '/path/to/tmp'
-    });
-  });
+	beforeEach(async () => {
+		builder = await initBuilder({
+			testDir: fixturePath(),
+			deviceOsDir: '/path/to/device-os',
+			tempDir: '/path/to/tmp'
+		});
+	});
 
-  afterEach(async () => {
-    sinon.restore();
-    await builder.shutdown();
-  });
+	afterEach(async () => {
+		sinon.restore();
+		await builder.shutdown();
+	});
 
-  describe('findApps()', () => {
-    it('finds application directories', async () => {
-      const dirs = builder.findApps('.'); // Relative to the test directory
-      expect(dirs).to.deep.equal(['suite_01', 'suite_02']);
-    });
-  });
+	describe('findApps()', () => {
+		it('finds application directories', async () => {
+			const dirs = builder.findApps('.'); // Relative to the test directory
+			expect(dirs).to.deep.equal(['suite_01', 'suite_02']);
+		});
+	});
 });
